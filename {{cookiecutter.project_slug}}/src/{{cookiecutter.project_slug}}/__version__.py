bd9187f3bf0e6c226d32404133132a8a92fac761
Gonzalo Casas <casas@arch.ethz.ch>
2018-08-04 19:38:21 +0200

    Move sample module outside `__init__.py`

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__version__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__version__.py
@@ -6,3 +6,5 @@ __author__ = 'Gramazio Kohler Research'
 __author_email__ = 'gramaziokohler@arch.ethz.ch'
 __license__ = 'MIT license'
 __copyright__ = 'Copyright {% now 'utc', '%Y' %} Gramazio Kohler Research'
+
+__all__ = ['__author__', '__author_email__', '__copyright__', '__description__', '__license__', '__title__', '__url__', '__version__']
